d3a02bef03a1c4cbbebdb3e84af107885fb8ea6f
Vtom7 <toms.avenge02@gmail.com>
2020-09-03 16:55:03 +0200

    creation.h

---
+++ projet_initial.h
@@ -0,0 +1,11 @@
+#ifndef PROJET_INITIAL_H
+#define PROJET_INITIAL_H
+
+#include "Projet_Initial_global.h"
+
+class PROJET_INITIALSHARED_EXPORT Projet_Initial {
+public:
+    Projet_Initial();
+};
+
+#endif // PROJET_INITIAL_H
